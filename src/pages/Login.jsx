aa1515cd5971b0d90c753178a1d1154b434c231e
Ch Lokesh <lokeshchirumamilla2104@gmail.com>
2025-07-15 15:23:34 +0530

    enhanced login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,13 +22,31 @@ export default function Login() {
 
   return (
     <div className="p-8 max-w-md mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      {err && <p className="text-red-500 text-sm">{err}</p>}
-      <form onSubmit={handleLogin} className="flex flex-col gap-4">
-        <input className="p-2 border rounded" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-        <input className="p-2 border rounded" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">Login</button>
-      </form>
+      <div className="flex-col justify-center items-center mt-10">
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+        {err && <p className="text-red-500 text-sm">{err}</p>}
+        <form onSubmit={handleLogin} className="flex flex-col gap-4">
+          <input
+            className="p-2 border rounded"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+          <input
+            className="p-2 border rounded"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+          <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-600">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
