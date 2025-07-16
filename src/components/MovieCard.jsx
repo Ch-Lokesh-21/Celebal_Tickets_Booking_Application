1673b76d059ff1ad83a4f57ccc5f8aa44a5e9f59
Ch Lokesh <lokeshchirumamilla2104@gmail.com>
2025-07-16 09:28:01 +0530

    updated movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function MovieCard({ movie }) {
   return (
-    <div className="border rounded p-4 shadow-lg hover:shadow-xl">
+    <div className="border-none rounded p-4 shadow-2xl hover:scale-105 transition-normal duration-300">
       <img src={movie.poster} alt={movie.title} className="w-full h-48 object-cover rounded" />
       <h3 className="font-semibold text-lg mt-2">{movie.title}</h3>
       <p className="text-sm text-gray-600">{movie.description}</p>
       <p className="text-sm mt-1">{movie.date} @ {movie.time}</p>
       <Link to={`/show/${movie.id}`}>
-        <button className="bg-blue-600 text-white mt-3 px-4 py-2 rounded w-full">Book Now</button>
+        <button className="bg-blue-600 text-white mt-3 px-4 py-2 rounded w-full cursor-pointer">Book Now</button>
       </Link>
     </div>
   );
